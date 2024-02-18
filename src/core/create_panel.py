10c4a2aea9811a6d01e129e7dc5be65775ce6f58
killown <systemofdown@gmai.com>
2024-02-18 04:22:22 -0300

    more monitor info fix

--- src/core/create_panel.py
+++ src/core/create_panel.py
@@ -1,17 +1,69 @@
 import gi
+import os
+import toml
 
 gi.require_version("Adw", "1")
 gi.require_version("Gtk4LayerShell", "1.0")
 from gi.repository import Gtk4LayerShell as LayerShell
-from gi.repository import Adw
+from gi.repository import Adw, Gdk
+
+
+def get_monitor_info():
+    """
+    Retrieve information about the connected monitors.
+
+    This function retrieves information about the connected monitors,
+    such as their dimensions and names,
+    and returns the information as a dictionary.
+
+    Returns:
+        dict: A dictionary containing information
+        about the connected monitors.
+    """
+    # get default display and retrieve
+    # information about the connected monitors
+    screen = Gdk.Display.get_default()
+    monitors = screen.get_monitors()
+    monitor_info = {}
+    for monitor in monitors:
+        monitor_width = monitor.get_geometry().width
+        monitor_height = monitor.get_geometry().height
+        name = monitor.props.connector
+        monitor_info[name] = {
+            "monitor": monitor,
+            "width": monitor_width,
+            "height": monitor_height,
+        }
+    return monitor_info
 
 
 def CreatePanel(app, anchor, layer, exclusive, width, height, class_style):
     window = Adw.Window(application=app)
-    window.set_default_size(width, height)
     window.add_css_class(class_style)
 
+    # lets try to set monitor info from Gdk, if not, get the panel default info instead
+    monitor = get_monitor_info()
+    gdk_monitor = None
+    monitor_name = None
+    home = os.path.expanduser("~")
+    config_path = os.path.join(home, ".config/waypanel")
+    panel_config = os.path.join(config_path, "panel.toml")
+    with open(panel_config) as panel_config:
+        config = toml.load(panel_config)
+
+    # Monitor dimensions to set the panel size
+    if "monitor" in config:
+        monitor_name = config["monitor"]["name"]
+
+    if monitor_name in monitor:
+        monitor = monitor[monitor_name]
+        gdk_monitor = monitor["monitor"]
+
+    window.set_default_size(width, height)
     LayerShell.init_for_window(window)
+
+    if gdk_monitor is not None:
+        LayerShell.set_monitor(window, gdk_monitor)
 
     if layer == "TOP":
         LayerShell.set_layer(window, LayerShell.Layer.TOP)
