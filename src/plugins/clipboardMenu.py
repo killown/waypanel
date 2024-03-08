7e2a0c27cb84393abd1c5cf2e1641baf74be35de
killown <systemofdown@gmai.com>
2024-03-07 21:05:07 -0300

    some popup fixes

--- src/plugins/clipboardMenu.py
+++ src/plugins/clipboardMenu.py
@@ -152,21 +152,13 @@ class MenuClipboard(Adw.Application):
         self.utils.run_app(cmd)
         self.popover_launcher.popdown()
 
-    def open_popover_launcher(self, *_):
-        LayerShell.set_keyboard_mode(self.top_panel, LayerShell.KeyboardMode.ON_DEMAND)
-
-        if self.popover_launcher and self.popover_launcher.is_visible():
-            self.popover_launcher.popdown()
-
-        self.create_popover_launcher(self.app)
-
     def open_popover_clipboard(self, *_):
-        LayerShell.set_keyboard_mode(self.top_panel, LayerShell.KeyboardMode.ON_DEMAND)
-
         if self.popover_clipboard and self.popover_clipboard.is_visible():
             self.popover_clipboard.popdown()
-
-        self.create_popover_clipboard(self.app)
+        if self.popover_clipboard and not self.popover_clipboard.is_visible():
+            self.popover_clipboard.popup()
+        if not self.popover_clipboard:
+            self.popover_clipboard = self.create_popover_clipboard(self.app)
 
     def popover_is_closed(self, *_):
         LayerShell.set_keyboard_mode(self.top_panel, LayerShell.KeyboardMode.NONE)
