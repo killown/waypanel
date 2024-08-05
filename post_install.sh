0c963f09e873d1d8f80a306a465829e30c2b5129
killown <systemofdown@gmai.com>
2024-08-04 22:40:16 -0300

    adjustments to work with pypi.org

--- post_install.sh
+++
@@ -1,7 +0,0 @@
-echo "Running post-install script..."
-cd /tmp/
-git clone https://github.com/wmww/gtk4-layer-shell
-cd gtk4-layer-shell/
-meson setup --prefix=~/.local/lib/gtk4-layer-shell -Dexamples=true -Ddocs=true -Dtests=true build
-ninja -C build
-ninja -C build install
