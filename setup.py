d9d4aad66618673e1470339f4d94d7e38421d1eb
killown <systemofdown@gmai.com>
2024-08-30 00:50:38 -0300

    new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name='waypanel',
-    version='0.10.0',
+    version='0.10.2',
     author='killown',
     author_email='systemofdown@gmail.com',
     description='A Wayfire panel that behaves like a shell',
