947d394f6c49bff6278dfd067b79462a6376063f
killown <systemofdown@gmai.com>
2025-04-11 03:09:15 -0300

    plugin example

---
+++ waypanel/src/plugins/example.py
@@ -0,0 +1,87 @@
+import gi
+import toml
+import os
+from gi.repository import Adw, Gtk
+from wayfire.ipc import WayfireSocket
+from wayfire.extra.ipc_utils import WayfireUtils
+
+
+def position():
+    # Define plugin position (left, right, center) and order
+    position = "right"
+    order = 10  # Higher number means lower priority
+    return position, order
+
+
+def initialize_plugin(obj, app):
+    """
+    Initialize the plugin.
+
+    obj: This is the main panel object from panel.py
+         It contains references to all panels (top, left, right, bottom)
+         and utility functions. You can access:
+         - obj.top_panel: The top panel window
+         - obj.utils: Utility functions for creating widgets, etc.
+         - obj.create_gesture(): To add gestures to panels
+
+    app: The main application instance
+    """
+
+    # The following will enable the plugin
+    # example = ExamplePluginFeatures()
+    # example.create_menu_popover_example(obj, app)
+
+
+class ExamplePluginFeatures(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.popover_example = None
+        self.app = None
+        self.top_panel = None
+        self.sock = WayfireSocket()
+        self.utils = WayfireUtils(self.sock)
+
+    def create_menu_popover_example(self, obj, app):
+        self.top_panel = obj.top_panel
+        self.app = app
+
+        # Setup basic button
+        self.menubutton_example = Gtk.Button()
+        self.menubutton_example.set_icon_name("preferences-system-symbolic")
+        self.menubutton_example.connect("clicked", self.open_popover_example)
+
+        # Add button to right panel
+        obj.top_panel_box_right.append(self.menubutton_example)
+
+        # Load custom configuration if exists
+        config_path = "~/.config/waypanel/waypanel.toml"
+        if os.path.exists(config_path):
+            with open(config_path, "r") as f:
+                config = toml.load(f)
+            custom_icon = config.get("icon", "preferences-system-symbolic")
+            self.menubutton_example.set_icon_name(custom_icon)
+
+        # Create system menu popover
+        self.popover_example = Gtk.Popover()
+        self.popover_example.set_parent(self.menubutton_example)
+        self.popover_example.set_has_arrow(False)
+
+        # Populate popover with widgets
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        # Add some example widgets
+        label = Gtk.Label(label="Example Plugin")
+        vbox.append(label)
+
+        switch = Gtk.Switch()
+        switch.set_active(False)
+        vbox.append(switch)
+
+        self.popover_example.set_child(vbox)
+
+    def open_popover_example(self, widget):
+        if self.popover_example:
+            self.popover_example.popup()
+
+    def popover_is_closed(self, *_):
+        print("Example plugin popover closed")
