c6d627d82b0a606ae8dd6572c1e082dffb6c7941
killown <systemofdown@gmai.com>
2025-05-04 18:08:17 -0300

    remove non-used features

--- waypanel/src/core/log_setup.py
+++ waypanel/src/core/log_setup.py
@@ -5,7 +5,6 @@ import structlog
 from structlog.stdlib import ProcessorFormatter, BoundLogger, add_logger_name
 from structlog.processors import JSONRenderer, TimeStamper, add_log_level
 from rich.logging import RichHandler
-import colorama
 
 # Define log file path
 LOG_FILE_PATH = os.path.expanduser("~/.config/waypanel/waypanel.log")
@@ -62,52 +61,17 @@ def setup_logging(level=logging.DEBUG) -> BoundLogger:
 
         logger.propagate = False  # Disable propagation to the root logger
 
-    cr = structlog.dev.ConsoleRenderer(
-        columns=[
-            # Render the timestamp without the key name in yellow.
-            structlog.dev.Column(
-                "timestamp",
-                structlog.dev.KeyValueColumnFormatter(
-                    key_style=None,
-                    value_style=colorama.Fore.YELLOW,
-                    reset_style=colorama.Style.RESET_ALL,
-                    value_repr=str,
-                ),
-            ),
-            # Render the event without the key name in bright magenta.
-            structlog.dev.Column(
-                "event",
-                structlog.dev.KeyValueColumnFormatter(
-                    key_style=None,
-                    value_style=colorama.Style.BRIGHT + colorama.Fore.MAGENTA,
-                    reset_style=colorama.Style.RESET_ALL,
-                    value_repr=str,
-                ),
-            ),
-            # Default formatter for all keys not explicitly mentioned. The key is
-            # cyan, the value is green.
-            structlog.dev.Column(
-                "",
-                structlog.dev.KeyValueColumnFormatter(
-                    key_style=colorama.Fore.CYAN,
-                    value_style=colorama.Fore.GREEN,
-                    reset_style=colorama.Style.RESET_ALL,
-                    value_repr=str,
-                ),
-            ),
-        ]
-    )
-
-    # Configure structlog processors
     # Configure structlog processors
     structlog.configure(
         processors=[
-            add_log_level,  # Add log level (info, debug, etc.)
-            add_logger_name,  # Add logger name
-            TimeStamper(fmt="iso"),  # Timestamp in ISO format
-            structlog.processors.StackInfoRenderer(),  # Include stack info if available
-            structlog.processors.format_exc_info,  # Pretty-print exceptions
-            cr if level <= logging.DEBUG else ProcessorFormatter.wrap_for_formatter,
+            add_log_level,
+            add_logger_name,
+            TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.dev.ConsoleRenderer()
+            if level <= logging.DEBUG
+            else ProcessorFormatter.wrap_for_formatter,
         ],
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
