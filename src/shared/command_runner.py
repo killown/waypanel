9b5be5db1089b3d880b62fccd84ccc1d4d4df359
Thiago <killown.matrix@gmail.com>
2026-01-13 08:50:11 -0300

    command_runner: use flatpak-spawn if flatpak-info exists

--- src/shared/command_runner.py
+++ src/shared/command_runner.py
@@ -7,23 +7,29 @@ class CommandRunner:
     def __init__(self, panel_instance):
         self.logger = panel_instance.logger
         self.ipc = panel_instance.ipc
+        self.is_flatpak = os.path.exists("/.flatpak-info")
 
     def run(self, cmd: str) -> None:
         """
         Execute a shell command without blocking the main GTK thread.
         """
         try:
+            final_cmd = cmd
+            if self.is_flatpak:
+                final_cmd = f"flatpak-spawn --host {cmd}"
+
             if os.getenv("WAYFIRE_SOCKET"):
 
-                def run():
-                    self.ipc.run_cmd(cmd)
+                def run_wayfire():
+                    self.ipc.run_cmd(final_cmd)
                     return False
 
-                GLib.idle_add(run)
+                GLib.idle_add(run_wayfire)
+
             if os.getenv("SWAYSOCK"):
                 GLib.idle_add(
                     lambda: subprocess.Popen(
-                        cmd,
+                        final_cmd,
                         shell=True,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
@@ -31,7 +37,7 @@ class CommandRunner:
                         start_new_session=True,
                     )
                 )
-                self.logger.info("Command scheduled for execution.")
+                self.logger.info(f"Command scheduled for execution: {final_cmd}")
         except Exception as e:
             self.logger.error(
                 error=e, message=f"Error running command: {cmd}", level="error"
