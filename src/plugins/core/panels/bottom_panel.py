68d81d6a2269432f58a8a33b1f29ef771f58cd8a
Thiago <killown.matrix@gmail.com>
2026-01-24 21:31:56 -0300

    bottom_panel: removed hover effect when entering an empty surface

--- src/plugins/core/panels/bottom_panel.py
+++ src/plugins/core/panels/bottom_panel.py
@@ -2,7 +2,7 @@ def get_plugin_metadata(_):
     return {
         "id": "org.waypanel.plugin.bottom_panel",
         "name": "Bottom Panel",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "enabled": True,
         "priority": 10,
         "container": "bottom-panel",
@@ -20,70 +20,63 @@ def get_plugin_class():
             self.add_css_class()
 
         def _setup_boxes(self):
+            # Left, Center, and Right Containers
             self.obj.bottom_panel_box_left = self.gtk.Box.new(
                 self.gtk.Orientation.HORIZONTAL, 6
             )
             self.obj.bottom_panel_box_center = self.gtk.Box.new(
-                self.gtk.Orientation.HORIZONTAL, 6
+                self.gtk.Orientation.HORIZONTAL, 0
             )
             self.obj.bottom_panel_box_right = self.gtk.Box.new(
                 self.gtk.Orientation.HORIZONTAL, 6
             )
-            self.obj.bottom_panel_box_left.set_hexpand(False)
-            self.obj.bottom_panel_box_right.set_hexpand(False)
-            self.obj.bottom_panel_box_center.set_hexpand(True)
-            self.obj.bottom_panel_box_center.set_halign(self.gtk.Align.FILL)
-            self.obj.bottom_panel_box_left.set_halign(self.gtk.Align.START)
-            self.obj.bottom_panel_box_right.set_halign(self.gtk.Align.END)
-            self.obj.bottom_panel_box_full = self.gtk.Grid()
-            self.obj.bottom_panel_box_full.set_column_homogeneous(False)
-            self.obj.bottom_panel_box_full.set_hexpand(True)
-            self.obj.bottom_panel_box_full.set_halign(self.gtk.Align.FILL)
-            self.obj.bottom_panel_box_full.attach(
-                self.obj.bottom_panel_box_left, 0, 0, 1, 1
+
+            self.obj.bottom_panel_center_box = self.gtk.CenterBox()
+            self.obj.bottom_panel_center_box.set_start_widget(
+                self.obj.bottom_panel_box_left
             )
-            self.obj.bottom_panel_box_full.attach_next_to(
-                self.obj.bottom_panel_box_center,
-                self.obj.bottom_panel_box_left,
-                self.gtk.PositionType.RIGHT,
-                1,
-                1,
+            self.obj.bottom_panel_center_box.set_center_widget(
+                self.obj.bottom_panel_box_center
             )
-            self.obj.bottom_panel_box_full.attach_next_to(
-                self.obj.bottom_panel_box_right,
-                self.obj.bottom_panel_box_center,
-                self.gtk.PositionType.RIGHT,
-                1,
-                1,
+            self.obj.bottom_panel_center_box.set_can_focus(False)
+            self.obj.bottom_panel_center_box.set_end_widget(
+                self.obj.bottom_panel_box_right
             )
-            self.main_widget = (self.obj.bottom_panel_box_full, "set_content")
+
+            self.obj.bottom_panel_scrolled_window = self.gtk.ScrolledWindow()
+            self.obj.bottom_panel_scrolled_window.set_policy(
+                self.gtk.PolicyType.AUTOMATIC, self.gtk.PolicyType.NEVER
+            )
+            self.obj.bottom_panel_scrolled_window.set_child(
+                self.obj.bottom_panel_center_box
+            )
+
+            self.obj.bottom_panel_scrolled_window.set_focus_on_click(False)
+            self.obj.bottom_panel_center_box.set_focus_on_click(False)
+
+            self.main_widget = (self.obj.bottom_panel_scrolled_window, "set_child")
 
         def add_css_class(self):
             """Add CSS classes once widgets are ready."""
-            if all(
-                self.gtk_helper.is_widget_ready(box)
-                for box in [
-                    self.obj.bottom_panel_box_left,
-                    self.obj.bottom_panel_box_center,
-                    self.obj.bottom_panel_box_right,
-                    self.obj.bottom_panel_box_full,
-                ]
-            ):
-                self.update_widget_safely(
-                    self.obj.bottom_panel_box_left.add_css_class,
-                    "bottom-panel-box-left",
+            widgets = [
+                self.obj.bottom_panel_box_left,
+                self.obj.bottom_panel_box_center,
+                self.obj.bottom_panel_box_right,
+                self.obj.bottom_panel_center_box,
+                self.obj.bottom_panel_scrolled_window,
+            ]
+
+            if all(self.is_widget_ready(w) for w in widgets):
+                self.obj.bottom_panel_box_left.add_css_class("bottom-panel-box-left")
+                self.obj.bottom_panel_box_center.add_css_class(
+                    "bottom-panel-box-center"
                 )
-                self.update_widget_safely(
-                    self.obj.bottom_panel_box_center.add_css_class,
-                    "bottom-panel-box-center",
+                self.obj.bottom_panel_box_right.add_css_class("bottom-panel-box-right")
+                self.obj.bottom_panel_center_box.add_css_class(
+                    "bottom-panel-center-box"
                 )
-                self.update_widget_safely(
-                    self.obj.bottom_panel_box_right.add_css_class,
-                    "bottom-panel-box-right",
-                )
-                self.update_widget_safely(
-                    self.obj.bottom_panel_box_full.add_css_class,
-                    "bottom-panel-box-full",
+                self.obj.bottom_panel_scrolled_window.add_css_class(
+                    "bottom-panel-scrolled-window"
                 )
                 return False
             else:
